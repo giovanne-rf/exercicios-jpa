58466e0faa50ab7e0c751bda379fa54a0aac7a91
Giovanne Alves <63433253+giovanne-rf@users.noreply.github.com>
2020-10-30 08:13:14 -0300

    Atualização da classe usuáreios

--- src/modelo/basico/Usuario.java
+++ src/modelo/basico/Usuario.java
@@ -1,47 +1,55 @@
 package modelo.basico;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Usuario {
-			@Id
-			@GeneratedValue (strategy = GenerationType.IDENTITY)
-			private Long Id;
-			private String nome;
-			private String email;
-			public Usuario() {
-		}
+	
+	@Id
+	@GeneratedValue
+	Long id;
+	
+	String nome;
+	
+	// @Transient atributo nao mapeado para o banco de dados (atributos calculados);
+	String email;
+	
+	public Usuario () {	
+	}
 
-		public Usuario(String nome, String email) {
-			this.nome = nome;
-			this.email = email;
-		}
+	public Usuario(String nome, String email) {
+		super();
+		this.nome = nome;
+		this.email = email;
+	}
 
-		public Long getId() {
-			return Id;
-		}
+	public Long getId() {
+		return id;
+	}
 
-		public void setId(Long id) {
-			Id = id;
-		}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-		public String getNome() {
-			return nome;
-		}
+	public String getNome() {
+		return nome;
+	}
 
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-		public String getEmail() {
-			return email;
-		}
+	public String getEmail() {
+		return email;
+	}
 
-		public void setEmail(String email) {
-			this.email = email;
-		}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 	
 
 }
